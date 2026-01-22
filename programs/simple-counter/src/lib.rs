19b8f4ce548fe39cdbf72e8d569044d3a6364826
Abhineet Biju <abhineet.biju19@gmail.com>
2026-01-22 18:15:59 +0530

    Build the Smart Contract for the program.

--- programs/simple-counter/src/lib.rs
+++ programs/simple-counter/src/lib.rs
@@ -7,10 +7,49 @@ pub mod simple_counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter;
+
+        counter.count = 0;
+
+        msg!("Counter account has been initialized!");
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        counter.count += 1;
+
+        msg!("Counter has been incremented!");
         Ok(())
     }
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub count: u32,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account( init,
+        space = 8+Counter::INIT_SPACE, payer=user,
+        seeds = [b"counter", user.key().as_ref()], bump)]
+    pub counter: Account<'info, Counter>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account()]
+    pub authority: Signer<'info>,
+
+    #[account(mut,
+        seeds=[b"counter", authority.key().as_ref()], bump)]
+    pub counter: Account<'info, Counter>,
+}
